2d6e64e922ae39b4aa44344e5ff7ef76b081f831
tania-developer <69915059+tania-developer@users.noreply.github.com>
2021-03-26 13:08:14 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('I know how to open node')
+});
+
+app.listen(4200,()=>console.log('listening on port 4200'));
